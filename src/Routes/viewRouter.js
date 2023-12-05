da68505f1c0af70dd79e699b85e0ad40a6ef7c82
mijael figueroa <figueroamijael86@gmail.com>
2023-12-05 19:45:02 -0300

    allData

---
+++ src/Routes/viewRouter.js
@@ -0,0 +1,112 @@
+import { Router } from "express";
+import { ProductManager, Productos } from '../ProductManager.js';
+
+
+const viewRouter = (socketServer, manager) => {
+  const router = Router();
+
+
+
+
+router.get("/", (req, res) => {
+  res.render("index", {
+    title: "Bienvenida",
+      fileCss: "styles.css",
+  })
+})
+
+// viewRouter.js
+router.get("/home", (req, res) => {
+  const productos = manager.mostrarProductos();
+  res.render("home", {
+      title: "Lista de Productos",
+      fileCss: "styles.css",
+      productos: productos,
+  });
+});
+
+
+router.get("/realtimeproducts", (req, res) => {
+  const productos = manager.mostrarProductos();
+  res.render("realTimeProducts", {
+      title: "Lista de Productos en Tiempo Real",
+      fileCss: "styles.css",
+      productos: productos,
+  });
+});
+
+
+const users = [];
+
+// Form
+router.get("/form", (req, res) => {
+  res.render("form", {
+    title: "Form example",
+    fileCss: "styles.css",
+  });
+});
+
+router.post("/user", (req, res) => {
+  const { name, age } = req.body;
+
+  users.push({
+    name,
+    age,
+  });
+
+  res.json({
+    message: "User created",
+    user: { name, age },
+  });
+});
+
+router.post("/realtimeproducts", (req, res) => {
+  const { title, description, price, category, thumbnail, stock, code } = req.body;
+
+  const product = new Productos(
+    title,
+    description,
+    price,
+    category,
+    thumbnail,
+    stock,
+    code
+  );
+
+  try {
+    manager.addProduct(product);
+    socketServer.emit("realTimeProducts_list", manager.mostrarProductos()); // Envía la lista actualizada a todos los clientes
+    res.status(201).json({
+      message: "Producto creado",
+      product,
+    });
+  } catch (e) {
+    res.status(500).json({
+      message: "Hubo un error al crear el nuevo producto",
+      error: e.message,
+    });
+  }
+});
+
+router.post("/deleteProduct", (req, res) => {
+  const { id } = req.body;
+
+  try {
+      manager.deleteProduct(id);
+      manager.broadcastProducts(); // Envía la lista actualizada a todos los clientes
+      res.status(200).json({
+          message: "Producto eliminado",
+      });
+  } catch (e) {
+      res.status(500).json({
+          message: "Hubo un error al eliminar el producto",
+          error: e.message,
+      });
+  }
+});
+
+return router
+}
+
+export default viewRouter;
+
---
+++ src/servidor.js
@@ -0,0 +1,76 @@
+import express from 'express';
+import handlebars from 'express-handlebars';
+import __dirname from './utils.js'
+import productsRoute from './Routes/productsRoute.js';
+import cartRoute from './Routes/cartRoute.js';
+import viewRouter from './Routes/viewRouter.js'
+import { ProductManager, Productos } from './ProductManager.js';
+
+import { Server } from 'socket.io'
+
+const app = express();
+const PORT = 8080;
+const httpServer = app.listen(PORT, () => console.log(`Server is running at http://localhost:${PORT}`));
+
+const socketServer = new Server(httpServer)
+
+const manager = new ProductManager('./Productos.json');
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+app.engine(
+    "hbs",
+    handlebars.engine({
+      // index.hbs
+      extname: "hbs",
+      // Plantilla principal
+      defaultLayout: "main",
+    })
+  );
+  
+  // Seteamos nuestro motor
+  app.set("view engine", "hbs");
+  app.set("views", `${__dirname}/views`);
+  
+  // Public
+  app.use(express.static(`${__dirname}/public`));
+
+  app.use('/api/productos', productsRoute);
+  app.use('/api/cart', cartRoute);
+  app.use("/api", viewRouter(socketServer, manager));
+
+const users = [];
+
+// Socket communication
+socketServer.on("connection", (socketClient) => {
+  console.log("Nuevo cliente conectado");
+
+  socketClient.on("message", (data) => {
+      console.log(data);
+  });
+
+  socketClient.emit("server_message", "Mensaje desde el servidor");
+
+  socketClient.broadcast.emit("message_all", `${socketClient.id} Conectado`);
+
+  socketServer.emit("message_all_2", "Mensaje a todos");
+
+  socketClient.on("form_message", (data) => {
+    console.log(data);
+    users.push(data);
+    socketClient.emit("users_list", users);
+  });
+
+  socketClient.emit("users_list", users);
+
+  socketClient.on("realTimeForm_message", (data) => {
+    console.log(data);
+    try {
+        manager.addProduct(new Productos(data.title, data.description, data.price, data.category, data.thumbnail, data.stock, data.code));
+        manager.broadcastProducts(); // Envía la lista actualizada a todos los clientes
+    } catch (e) {
+        console.error("Hubo un error al procesar el mensaje del formulario en tiempo real:", e.message);
+    }
+});
+  })
